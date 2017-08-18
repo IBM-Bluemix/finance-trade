a71c40eff9500529505ad51496058dd65a80db30
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-08-18 11:35:57 +0200

    automatic loading spinner

--- app/dashboard/js/app.js
+++ app/dashboard/js/app.js
@@ -1,9 +1,29 @@
 /*global angular*/
 (function () {
 
+  // listen for request sent over XHR and automatically show/hide spinner
+  angular.module('ngLoadingSpinner', [])
+    .directive('spinner', ['$http', function ($http) {
+      return {
+        link: function (scope, elm, attrs) {
+          scope.isLoading = function () {
+            return $http.pendingRequests.length > 0;
+          };
+          scope.$watch(scope.isLoading, function (loading) {
+            if (loading) {
+              document.getElementById('loadingProgress').style.visibility = "visible";
+            } else {
+              document.getElementById('loadingProgress').style.visibility = "hidden";
+            }
+          });
+        }
+      };
+    }]);
+
   // angular app initialization
   var app = angular.module('app', [
     'ngMaterial',
+    'ngLoadingSpinner',
     'ui.router',
     'md.data.table',
   ]);
