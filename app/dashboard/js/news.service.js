183191c685d0ab44600f940c01e1c9fb346d13e9
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-08-18 16:41:09 +0200

    show sentiment count badges

--- app/dashboard/js/news.service.js
+++ app/dashboard/js/news.service.js
@@ -10,7 +10,13 @@
         console.log('Looking for', riskFactor, horizon);
         var deferred = $q.defer();
         $http.get('api/news.json').then(function(response) {
-          deferred.resolve(response.data);
+          var result = response.data;
+          result.counts = {
+            positive: result.results.filter(function(article) { return article.enriched_text.sentiment.document.label === 'positive' }).length,
+            negative: result.results.filter(function(article) { return article.enriched_text.sentiment.document.label === 'negative' }).length,
+            neutral: result.results.filter(function(article) { return article.enriched_text.sentiment.document.label === 'neutral' }).length,
+          };
+          deferred.resolve(result);
         }).catch(function(err) {
           console.log(err);
           deferred.reject();
